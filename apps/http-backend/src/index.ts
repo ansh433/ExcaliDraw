1537c5cc94f431a940545638d3e37a3bda53e199
ansh433 <Fantastic4303@gmail.com>
2026-02-10 03:38:36 +0530

    added db integration

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,6 +3,7 @@ import jwt from 'jsonwebtoken'
 import {JWT_SECRET} from  '@repo/backend-common/config'
 import { middleware } from "./middleware";
 import {CreateUserSchema} from "@repo/common/types"
+import {SigninSchema} from "@repo/common/types"
 
 
 
@@ -10,9 +11,35 @@ const app = express ();
 
 app.post("/signup", (req, res)=> {
 
+    const data = CreateUserSchema.safeParse(req.body);
+
+    if(!data.success){
+        res.json({
+            message: "Incorrect inputs"
+        })
+        return;
+    }
+
+    res.json({
+        userId: "123"
+    })
+
 })
 
 app.post("signin", (req, res) =>{
+
+    const data = SigninSchema.safeParse(req.body);
+
+    if(!data.success){
+        res.json({
+            message: "Incorrect inputs"
+        })
+        return;
+    }
+
+    res.json({
+        userId: "123"
+    })
 
 })
 
